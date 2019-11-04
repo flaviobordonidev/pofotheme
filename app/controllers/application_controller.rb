181a8cf26d408c18d12d3dfdebfbd5cd82238ca4
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-11-04 09:38:44 +0000

    set i18n change language on url via params[:locale]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  #-----------------------------------------------------------------------------
+  private
+  
+  #set language for internationalization
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
 end
