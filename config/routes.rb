290c66924ebd6bd06fb3cd2696a36ad49b96df26
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-11-05 15:36:16 +0000

    Implement devise with User model and table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'mockups#page_a'
 
+  devise_for :users
+  resources :users
+
   get 'mockups/page_a'
   get 'mockups/page_b'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
