fdc8258e5fc1c6d347c14807b9834dc6dc974130
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-11-11 14:17:10 +0000

    Implement views to manage users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_135031) do
+ActiveRecord::Schema.define(version: 2019_11_09_221023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "eg_users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "encrypted_password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
