a0f2b429e1fa55271afe89619db920370dbbe345
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-11-20 14:29:40 +0000

    Implement devise protection to users and more

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  def after_sign_in_path_for(resource_or_scope)
+    current_user # goes to users/1 (if current_user = 1)
+    #users_path #goes to users/index
+  end
+
   #-----------------------------------------------------------------------------
   private
   
