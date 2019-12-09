541e27e99b454308c5c8db35b60ff2878ba17e6b
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-12-09 08:42:57 +0000

    add seed people

--- db/seeds/eg_posts.rb
+++ db/seeds/eg_posts.rb
@@ -1,18 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-# https://medium.com/@ethanryan/split-your-rails-seeds-file-into-separate-files-in-different-folders-3c57be765818
-
-#Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
-#  load seed
-#end
-
 puts "Inseriamo tre articoli per tre utenti"
 
 u1 = User.find(1)
