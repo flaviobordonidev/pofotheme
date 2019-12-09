2d3ce69b243aa2a4156581a158e8721ac7ff4538
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-12-09 10:01:10 +0000

    Add table eg_companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_115053) do
+ActiveRecord::Schema.define(version: 2019_12_09_095017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "eg_companies", force: :cascade do |t|
+    t.string "name"
+    t.text "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "eg_posts", force: :cascade do |t|
     t.string "meta_title"
